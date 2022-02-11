665b9dc808c6e6444c747f2d989402a39ffc2d1d
Jose Corzon <jose_corzon@yahoo.com>
2022-02-11 17:38:40 -0500

    counter

---
+++ Python/flask/counter/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+
+@app.route('/')
+def index():
+    if "number" not in session:
+        session["number"] = 1
+    else:
+        session["number"] += 1
+    return render_template('index.html')
+
+
+@app.route('/destroy_session')
+def destroy():
+    session.clear()
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
