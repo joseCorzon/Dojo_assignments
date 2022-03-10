793896443e6a593d9dabdd301a558bf503246697
Jose Corzon <jose_corzon@yahoo.com>
2022-03-10 15:40:12 -0500

    hook form

---
+++ MERN/react/hook_form/hook-form/src/components/HookForm.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from  'react';
+
+const HookForm = ()=>{
+    let [firstName, setFirstName] = useState('')
+    let [lastName, setLastName] = useState('')
+    let [email, setEmail] = useState('')
+    let [password, setPassword] = useState('')
+    let [confirmPassword, setConfirmPassword] = useState('')
+
+    return (
+        <form>
+            <div claseName='form-group'>
+                <label htmlFor=''>First Name</label>
+                <input type='text' clasName='form-control' onChange={(e)=>setFirstName(e.target.value)}></input>
+            </div>
+            <div claseName='form-group'>
+                <label htmlFor=''>Last Name</label>
+                <input type='text' clasName='form-control' onChange={(e)=>setLastName(e.target.value)}></input>
+            </div>
+            <div claseName='form-group'>
+                <label htmlFor=''>Email</label>
+                <input type='email' clasName='form-control' onChange={(e)=>setEmail(e.target.value)}></input>
+            </div>
+            <div claseName='form-group'>
+                <label htmlFor=''>Password</label>
+                <input type='password' clasName='form-control' onChange={(e)=>setPassword(e.target.value)}></input>
+            </div>
+            <div claseName='form-group'>
+                <label htmlFor=''>Confirm Password</label>
+                <input type='password' clasName='form-control' onChange={(e)=>setConfirmPassword(e.target.value)}></input>
+            </div>
+            <p>Your Form Data</p>
+            <p>First Name: {firstName}</p>
+            <p>Last Name: {lastName}</p>
+            <p>Email: {email}</p>
+            <p>Password: {password}</p>
+            <p>Confirm Password: {confirmPassword}</p>
+        </form>
+    )
+}
+
+export default HookForm;
