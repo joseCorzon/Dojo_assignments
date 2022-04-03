4e350146f506d764b753ed11bf78638ff32f88a0
Jose Corzon <jose_corzon@yahoo.com>
2022-04-03 14:06:13 -0400

    first java program

---
+++ java/first/First.java
@@ -0,0 +1,8 @@
+public class First {
+	public static void main(String[] args) {
+        System.out.println("My name is Jose Corzon");
+        System.out.println("I am 24 years old");
+        System.out.println("my hometown is Miami, FL");
+
+	}
+}
